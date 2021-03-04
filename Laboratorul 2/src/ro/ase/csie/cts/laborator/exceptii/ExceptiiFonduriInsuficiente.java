4f6f57b43eb0948d25c72dcc794f0c5edc14940b
dobramarian <dobramarian18@stud.ase.ro>
2021-03-04 12:24:20 +0200

    LAB 2

---
+++ Laboratorul 2/src/ro/ase/csie/cts/laborator/exceptii/ExceptiiFonduriInsuficiente.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.laborator.exceptii;
+
+public class ExceptiiFonduriInsuficiente {
+
+}
---
+++ Laboratorul 2/src/ro/ase/csie/cts/laborator/interfete/Profitabil.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.laborator.interfete;
+
+public interface Profitabil {
+	public static final int MAX_PROCENT_DOBANDA = 5;
+	public abstract void adaugaDobanda(double procentDobanda);
+	
+}
